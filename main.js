b203ba8824a250ce480a795f4eb1ff3b217a900d
ant-7802 <anttubemc@googlemail.com>
2024-01-21 20:51:25 -0500

    aya

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-particlesJS.load('particles-js', 'assets/particles.json', function() {
+particlesJS.load('particles-js', '/assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
 });
 
 document.addEventListener("DOMContentLoaded", (event) => {    
     var topbar = document.querySelector("#topbar");
-    fetch("topbar.html")
+    fetch("/topbar.html")
     .then((response) => response.text())
     .then((data) => {
         topbar.innerHTML = data
