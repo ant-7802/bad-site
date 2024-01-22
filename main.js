d21ea842a838058ee7bdce643cf17be2691f03e1
ant-7802 <anttubemc@googlemail.com>
2024-01-22 18:18:42 -0500

    ok

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 particlesJS.load('particles-js', '/assets/particles.json', function() {
-    console.log('callback - particles.js config loaded');
+    console.log("loaded super cool particle library that everyone uses for their sites :)))")
 });
 
 document.addEventListener("DOMContentLoaded", (event) => {    
@@ -10,4 +10,44 @@ document.addEventListener("DOMContentLoaded", (event) => {
         topbar.innerHTML = data
     })
 });
-  
+
+
+function login() {
+    fetch('/log', {
+     method: 'POST',
+     headers: {
+     'Content-Type': 'application/json'
+      },
+     body: JSON.stringify({
+      username: document.querySelector("#Username").value,
+      password: document.querySelector("#Password").value
+     })
+    })
+    .then(response => response.data)
+    .then(data => {
+        console.log(data)
+    })
+  .catch(error => console.error(error));
+
+}
+
+
+function register() {
+    fetch('/reggy', {
+     method: 'POST',
+     headers: {
+     'Content-Type': 'application/json'
+      },
+     body: JSON.stringify({
+      username: document.querySelector("#Username").value,
+      password: document.querySelector("#Password").value,
+      registercode: document.querySelector("#Registercode").value
+     })
+    })
+    .then(response => response.data)
+    .then(data => {
+        console.log(data)
+    })
+  .catch(error => console.error(error));
+
+}
